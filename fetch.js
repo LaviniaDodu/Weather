2542db184e61dc584572b0f8f3710ac100a447bc
LaviniaDodu <82836346+LaviniaDodu@users.noreply.github.com>
2021-05-19 01:45:58 +0200

    Modifiche lato CSS

--- fetch.js
+++ fetch.js
@@ -1,96 +1,112 @@
+
 class Fetch {
-  async getCurrent(input) {
-    const myKey = "cdb91b50203dabb6954097b1c812fa6d";
+    async getCurrent(input) {
+      const myKey = "cdb91b50203dabb6954097b1c812fa6d";
+  
+      //make request to url
+  
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&lang=it&units=metric&appid=${myKey}`
+      );
+  
+      const data = await response.json();
+  
 
-    //make request to url
-
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
-    );
-
-    const data = await response.json();
-
-    console.log(data);
-
-    return data;
+      console.log(data);
+  
+      return data;
+    }
   }
-}
-
-
-class UI {
-  constructor() {
-    this.uiContainer = document.querySelector("#content");
-    this.city;
-    this.defaultCity = "London";
-  }
-
-  populateUI(data) {
-    //de-structure vars
-
-    //add them to inner HTML
-
-    this.uiContainer.innerHTML = `
-        
-        <div class="card mx-auto response-weather">
-            <div class="card-body justify-content-center text-center">
-                <h4 class="card-title py-2 text-blue">${data.name}</h4>
-                <h6 class="card-subtitle text-muted py-2">Massime <strong class="text-yellow">${data.main.temp_max}</strong> <br>Minime <strong class="text-yellow">${data.main.temp_min}</strong></h6>
-                <p class="card-text pt-2">Le condizioni del tempo previste sono: <br> <strong class="text-yellow">${data.weather[0].description}</strong></p>
-                
+  
+  
+  class UI {
+    constructor() {
+      this.uiContainer = document.querySelector("#content");
+      this.city;
+      this.defaultCity = "London";
+    }
+  
+    populateUI(data) {
+      //de-structure vars
+  
+      //add them to inner HTML
+  
+      this.uiContainer.innerHTML = `
+          
+        <div class="card mx-auto response-weather" >
+            <div class="row justify-content-center align-items-center px-4 py-3">
+                <div class="col-6">
+                  <h2 class="card-title py-2">${data.name}</h2>
+                </div>
+                <div class="col-6 output">
+                <p class="card-text pt-2 fs-5">Le condizioni del tempo previste sono: <br> 
+                <div class="text-center">
+                  <strong class="text-yellow"><h4>${data.weather[0].description}</h4></strong>
+                </div>
+                  </p>  
+                  <div class="">
+                    <h4 class="card-subtitle text-light py-2">Massime <strong class="ms-4 text-main">${data.main.temp_max} °C</strong> <br>Minime <strong class="ms-4 text-main">${data.main.temp_min} °C</strong></h4>
+                  </div>
+                </div>  
             </div>
         </div>
-        
-        
-        `;
-  }
-
-  clearUI() {
-    uiContainer.innerHTML = "";
-  }
-
-  saveToLS(data) {
-    localStorage.setItem("city", JSON.stringify(data));
-  }
-
-  getFromLS() {
-    if (localStorage.getItem("city" == null)) {
-      return this.defaultCity;
-    } else {
-      this.city = JSON.parse(localStorage.getItem("city"));
+          
+          
+          `;
     }
-
-    return this.city;
+  
+    clearUI() {
+      uiContainer.innerHTML = "";
+    }
+  
+    saveToLS(data) {
+      localStorage.setItem("city", JSON.stringify(data));
+    }
+  
+    getFromLS() {
+      if (localStorage.getItem("city" == null)) {
+        return this.defaultCity;
+      } else {
+        this.city = JSON.parse(localStorage.getItem("city"));
+      }
+  
+      return this.city;
+    }
+  
+    clearLS() {
+      localStorage.clear();
+    }
   }
+  
+  
+  //inst classes//
+  
+  const ft = new Fetch();
+  const ui = new UI();
+  
+  //add event listeners//
+  const search = document.querySelector("#searchUser");
+  const button = document.querySelector("#submit");
+  button.addEventListener("click", () => {
+  const currentVal = search.value;
 
-  clearLS() {
-    localStorage.clear();
-  }
-}
-
-
-//inst classes//
-
-const ft = new Fetch();
-const ui = new UI();
-
-//add event listeners//
-
-const search = document.querySelector("#searchUser");
-const button = document.querySelector("#submit");
-button.addEventListener("click", () => {
-const currentVal = search.value;
-
-ft.getCurrent(currentVal).then((data) => {
-  //call a UI method//
-  ui.populateUI(data);
-  //call saveToLS
-  ui.saveToLS(data);
-});
-});
-
-//event listener for local storage
-
-window.addEventListener("DOMContentLoaded", () => {
-const dataSaved = ui.getFromLS();
-ui.populateUI(dataSaved);
-});
+  
+  
+  
+  ft.getCurrent(currentVal).then((data) => {
+    //call a UI method//
+    ui.populateUI(data);
+    //call saveToLS
+    ui.saveToLS(data);
+  });
+  });
+  
+  //event listener for local storage
+  
+  window.addEventListener("DOMContentLoaded", () => {
+  const dataSaved = ui.getFromLS();
+  ui.populateUI(dataSaved);
+  });
+  
+  
+  
